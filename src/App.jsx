96381190907407fdb73327f1015d0f8de70cebfd
no0bie-jl <johnlyodsembilla02@gmail.com>
2025-02-07 03:07:47 +0800

    added icon profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import { useState } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
 function App() {
+  const [preview, setPreview] = useState(null);
   const [dataForm, setDataForm] = useState({
     firstname: "",
     lastname: "",
     middlename: "",
     email: "",
     phone: "",
-    profile_picture: null
+    profile_picture: null,
   });
 
   const fieldChange = (e) => {
     const { name, value } = e.target;
     setDataForm((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const fileChange = (e) => {
     const file = e.target.files[0];
-    setDataForm((prevData) => ({
-      ...prevData,
-      profile_picture: file
-    }));
+    if (file) {
+      setPreview(URL.createObjectURL(file)); // Set preview
+      setDataForm((prevData) => ({
+        ...prevData,
+        profile_picture: file,
+      }));
+    }
   };
 
   const submit = async (e) => {
@@ -55,102 +60,116 @@ function App() {
   };
 
   return (
-    <>
-      <div className='mt-5 container'>
-          <h3 className='text-center mb-4 mt-5 control-heading'>A simple user registration </h3>
-          <form onSubmit={submit} encType="multipart/form-data">
-            <div className="row d-flex flex-column align-items-center">
-              <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
-                <input
-                  id='profile_picture_input'
-                  type="file" 
-                  name="profile_picture" 
-                  accept="image/png,image/jpeg" 
-                  onChange={fileChange} 
-                  className="form-control d-none" 
-                />
-                <label htmlFor="profile_picture_input" className="form-control text-center btn btn-light">
-                  Select Profile Picture
-                </label>
-              </div>
-
-              <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
-                <input 
-                  type="text" 
-                  name="firstname" 
-                  id="u_fname" 
-                  placeholder='First Name' 
-                  value={dataForm.firstName} 
-                  onChange={fieldChange} 
-                  required 
-                  className="form-control" 
-                />
-              </div>
-
-              <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
-                <input 
-                  type="text" 
-                  name="lastname" 
-                  id="u_lname" 
-                  placeholder='Last Name' 
-                  value={dataForm.lastname} 
-                  onChange={fieldChange} 
-                  required 
-                  className="form-control" 
-                />
-              </div>
-
-              <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
-                <input 
-                  type="text" 
-                  name="middlename" 
-                  id="u_mname" 
-                  placeholder='Middle Name' 
-                  value={dataForm.middlename} 
-                  onChange={fieldChange} 
-                  required 
-                  className="form-control" 
-                />
-              </div>
-
-              <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
-                <input 
-                  type="email" 
-                  name="email" 
-                  id="u_email" 
-                  placeholder='Email' 
-                  value={dataForm.email} 
-                  onChange={fieldChange} 
-                  required 
-                  className="form-control" 
-                />
-              </div>
-
-              <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
-                <input 
-                  type="tel" 
-                  pattern="^\d{11}$" 
-                  placeholder="e.g. 09123456789" 
-                  name="phone" 
-                  id="u_phone" 
-                  value={dataForm.phone} 
-                  onChange={fieldChange} 
-                  required 
-                  className="form-control" 
-                />
-              </div>
-
-              <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3 d-flex justify-content-center">
-                <input 
-                  type="submit" 
-                  className='btn btn-primary w-50' 
-                  value={'Register'} 
-                />
-              </div>
+    <div className="mt-5 container">
+      <h3 className="text-center mb-4 mt-5 control-heading">
+        A simple user registration
+      </h3>
+      <form onSubmit={submit} encType="multipart/form-data">
+        <div className="row d-flex flex-column align-items-center">
+          <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
+            <div className="d-flex justify-content-center">
+              <label
+                htmlFor="profile_picture_input"
+                className="position-relative rounded-circle overflow-hidden border border-secondary d-flex justify-content-center align-items-center"
+                style={{ width: "180px", height: "180px", cursor: "pointer" }}
+              >
+                {preview ? (
+                  <img
+                    src={preview}
+                    alt="Preview"
+                    className="w-100 h-100 object-fit-cover"
+                  />
+                ) : (
+                  <span className="text-secondary">PROFILE</span>
+                )}
+              </label>
             </div>
-          </form>
+            <input
+              id="profile_picture_input"
+              type="file"
+              name="profile_picture"
+              accept="image/png,image/jpeg"
+              onChange={fileChange}
+              className="d-none"
+            />
+          </div>
+
+          <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
+            <input
+              type="text"
+              name="firstname"
+              id="u_fname"
+              placeholder="First Name"
+              value={dataForm.firstname}
+              onChange={fieldChange}
+              required
+              className="form-control"
+            />
+          </div>
+
+          <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
+            <input
+              type="text"
+              name="lastname"
+              id="u_lname"
+              placeholder="Last Name"
+              value={dataForm.lastname}
+              onChange={fieldChange}
+              required
+              className="form-control"
+            />
+          </div>
+
+          <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
+            <input
+              type="text"
+              name="middlename"
+              id="u_mname"
+              placeholder="Middle Name"
+              value={dataForm.middlename}
+              onChange={fieldChange}
+              required
+              className="form-control"
+            />
+          </div>
+
+          <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
+            <input
+              type="email"
+              name="email"
+              id="u_email"
+              placeholder="Email"
+              value={dataForm.email}
+              onChange={fieldChange}
+              required
+              className="form-control"
+            />
+          </div>
+
+          <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3">
+            <input
+              type="tel"
+              pattern="^\d{11}$"
+              placeholder="e.g. 09123456789"
+              name="phone"
+              id="u_phone"
+              value={dataForm.phone}
+              onChange={fieldChange}
+              required
+              className="form-control"
+            />
+          </div>
+
+          <div className="col-12 col-lg-4 col-md-5 col-sm-6 mb-3 d-flex justify-content-center">
+            <input
+              type="submit"
+              className="btn btn-primary w-50"
+              value="Register"
+            />
+          </div>
         </div>
-    </>
+      </form>
+    </div>
   );
 }
 
