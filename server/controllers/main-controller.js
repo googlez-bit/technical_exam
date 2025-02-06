63672bc7c4768ad9c5f58dab1e3042a9c11a10e5
no0bie-jl <johnlyodsembilla02@gmail.com>
2025-02-07 03:12:55 +0800

    change the message after registration

--- server/controllers/main-controller.js
+++ server/controllers/main-controller.js
@@ -6,7 +6,7 @@ exports.registerUser = async (req, res) => {
     const profile_picture = req.file ? req.file.filename : "";
     
     const newUser = await User.create({ firstname, lastname, middlename, email, phone, profile_picture });
-    res.status(201).json({ message: "User registered successfully", user: newUser });
+    res.status(201).json({ message: "Congratulations, you have successfully registered for an account!", user: newUser });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
